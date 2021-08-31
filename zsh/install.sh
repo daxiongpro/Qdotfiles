b5201937bf6d0aceed0afb8f140bb3ee9a021089
daxiongpro <910660298@qq.com>
2021-08-31 15:08:16 +0800

    Don't need sudo

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,7 +10,7 @@ elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
   # linux installation branch
   wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -qO - | zsh
   cp ~/.Qdotfiles/zsh/.zshrc ~/.zshrc
-  sudo chsh $USER -s $(which zsh)
+  chsh $USER -s $(which zsh)
 fi
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh && source ~/.Qdotfiles/zsh/theme.zsh
