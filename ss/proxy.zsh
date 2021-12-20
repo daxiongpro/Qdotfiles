812f804afe2d787bfb550cd4d7072fe05c4f0646
daxiongpro <910660298@qq.com>
2021-12-20 21:09:24 +0800

    update:proxy

--- ss/proxy.zsh
+++ ss/proxy.zsh
@@ -20,12 +20,11 @@ function cg(){
 }
 function _proxy_help(){
 cat << EOF
-Usage: proxy [on|off|up|down|status|restart|cmd|set|which|test]
-Command:
- - on/off: set http/https proxy port
- - on nbu/geely/wsl: set http proxy to my nbu/geely/wsl windows clash
- - up/down/restart: set docker based service of command line proxy
- - set: select one from all ss configurations which are in ~/.Qdotfiles/ss/export.json
+	Usage: proxy [on|off|up|down|status|restart|cmd|set|which|test]
+	Command:
+	- on/off: set http/https proxy port
+	- up/down/restart: set docker based service of command line proxy
+	- set: select one from all ss configurations which are in ~/.Qdotfiles/ss/export.json
 EOF
 }
 function proxy(){
@@ -44,15 +43,6 @@ function proxy(){
 			PROXY_PORT=$MAC_PROXY_PORT ;;
 		l*|linux)
 			PROXY_PORT=$LINUX_PROXY_PORT ;;
-		# nbu)
-		# 	PROXY_IP=10.22.148.80 
-		# 	PROXY_PORT=7890 ;;
-		# geely)
-		# 	PROXY_IP=192.168.4.148
-		# 	PROXY_PORT=7890 ;;
-		# wsl)
-		# 	PROXY_IP=127.0.0.1
-		# 	PROXY_PORT=7890 ;;
 		-h|--help)
 			echo "proxy on [OPTIONS]"
 			echo "Usage: set http/https proxy port"
@@ -91,60 +81,11 @@ function proxy(){
 		echo "Network test"
 		echo "--------------"
 		proxy test
-	elif [ "$1" = "up" ]; then
-		cd ~/.Qdotfiles
-		if [ "$2" = "-v" ]; then
-			docker-compose up --remove-orphans
-		elif [ "$2" = "-h" ]; then
-			echo "proxy up [-vh]"
-			echo "-v show debug info"
-		else
-			docker-compose up -d --remove-orphans
-		fi
-		cd - > /dev/null
-	elif [ "$1" = "down" ]; then
-		cd ~/.Qdotfiles
-		docker-compose down --remove-orphans
-		cd - > /dev/null
-	elif [ "$1" = "in" ]; then
-		cd ~/.Qdotfiles
-		docker-compose exec Qlinux zsh
-		cd - > /dev/null
-	elif [ "$1" = "restart" ] || [ "$1" = "r" ]; then
-		cd ~/.Qdotfiles
-		docker-compose restart
-	 	cd - > /dev/null
-	elif [ "$1" = "onn" ]; then
-		PROXY_PORT="${2:-${PROXY_PORT}}"
-		export http_proxy="http://127.0.0.1:$PROXY_PORT"
-		export https_proxy="https://127.0.0.1:$PROXY_PORT"
-	elif [ "$1" = "cmd" ];then
-		echo "Command to open http/s proxy"
-		echo export http_proxy="127.0.0.1:$PROXY_PORT"
-		echo export https_proxy="127.0.0.1:$PROXY_PORT"
-		echo
-		echo "Command to close http/s proxy"
-		echo export http_proxy=
-		echo export https_proxy=
-	elif [ "$1" = "which" ];then
-		cat ~/.Qdotfiles/ss/ss.json
+
 	elif [ "$1" = "test" ];then
 		cg
-	elif [ "$1" = "set" ];then
-		change_ss "${@:2}"
 	else
 		_proxy_help
 	fi
 }
 
-function change_ss(){
-	cd ~/.Qdotfiles/ss
-	PYTHON_VERSION=$(python -V | awk -F' ' '{print $2}' | awk -F'.' '{print $1}')
-	if command -v  python3  &> /dev/null
-	then
-		python3 update_ss.py "$@"
-	else
-		[[ $(PYTHON_VERSION) = "3" ]] && python update_ss.py "$@" || exit 1
-	fi
-	cd - > /dev/null
-}
