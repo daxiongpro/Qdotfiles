8d9d80f315e2c45d6130359ba9aab72436e38e8f
daxiongpro <910660298@qq.com>
2021-12-31 21:49:51 +0800

    bugfix:change gcc-9 to gcc-7

--- scripts/init_a_fresh_ubuntu/7.cuda10_install.sh
+++ scripts/init_a_fresh_ubuntu/7.cuda10_install.sh
@@ -4,8 +4,14 @@ if [ ! -d ~/Downloads ]; then
 	mkdir ~/Downloads
 fi
 cd ~/Downloads
+# gcc-9为默认值，应该修改为gcc-7
+sudo apt install gcc-7
+sudo rm /usr/bin/gcc
+sudo ln -s /usr/bin/gcc-7 /usr/bin/gcc
+gcc -v
 
 
+# 下载安装
 wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run
 sudo sh cuda_10.1.243_418.87.00_linux.run
 
