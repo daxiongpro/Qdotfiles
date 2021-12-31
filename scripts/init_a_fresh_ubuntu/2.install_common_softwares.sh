6f7c3701e2c5ff5562d3c288995cd1e0268ff234
daxiongpro <910660298@qq.com>
2021-12-31 20:56:12 +0800

    bugfix:install zsh

--- scripts/init_a_fresh_ubuntu/2.install_common_softwares.sh
+++ scripts/init_a_fresh_ubuntu/2.install_common_softwares.sh
@@ -3,7 +3,7 @@ if test "$(uname)" = "Darwin"; then
   echo not supported now on mac now!
 
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
-  softwares="git vim zsh silversearcher-ag\
+  softwares="git tmux vim autojump zsh silversearcher-ag\
 		       ranger curl tree make htop nodejs npm\
 			   snapd docker docker.io docker-compose"
   echo "Hi handsome user! I will install some very common softwares\
--- scripts/init_a_fresh_ubuntu/3.oh-my-zsh_install.sh
+++ scripts/init_a_fresh_ubuntu/3.oh-my-zsh_install.sh
@@ -4,11 +4,11 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # cp ~/.Qdotfiles/zsh/.zshrc ~/.zshrc
 
-# chsh -s /bin/zsh
+chsh -s /usr/bin/zsh
 
 # cd $ZSH_CUSTOM/plugins
 cd ~/.oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
-# source ~/.zshrc
+source ~/.zshrc
