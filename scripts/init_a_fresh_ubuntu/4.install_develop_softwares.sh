0c1e044cab88839899d1520ecf5bec128a8d536b
daxiongpro <910660298@qq.com>
2021-12-31 21:18:36 +0800

    update:chrome install

--- scripts/init_a_fresh_ubuntu/4.install_develop_softwares.sh
+++ scripts/init_a_fresh_ubuntu/4.install_develop_softwares.sh
@@ -1,5 +1,13 @@
 # sudo snap install pycharm-professional --classic
-sudo snap install pycharm-community --classic
-sudo snap install code --classic
+# sudo snap install pycharm-community --classic
+# sudo snap install code --classic
 
 # sudo snap install firefox chromium typora meshlab
+
+if [ ! -d ~/Downloads ]; then
+    mkdir ~/Downloads
+fi
+cd ~/Downloads
+# chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb 
