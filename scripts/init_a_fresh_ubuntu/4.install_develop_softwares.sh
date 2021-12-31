809672af1c2daa0e294abe508db675abe063edfb
daxiongpro <910660298@qq.com>
2021-12-31 22:19:35 +0800

    update:add wps install

--- scripts/init_a_fresh_ubuntu/4.install_develop_softwares.sh
+++ scripts/init_a_fresh_ubuntu/4.install_develop_softwares.sh
@@ -9,6 +9,11 @@ if [ ! -d ~/Downloads ]; then
 fi
 cd ~/Downloads
 # chrome
-rm google*
+rm *google*
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb 
+
+# wps
+rm *wps*
+wget https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/10702/wps-office_11.1.0.10702_amd64.deb
+sudo dpkg -i wps-office_11.1.0.10702_amd64.deb
