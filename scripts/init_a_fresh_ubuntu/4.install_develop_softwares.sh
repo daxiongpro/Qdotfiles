3ebb8f391a89ad4625f237a225811a1ace1ca940
daxiongpro <910660298@qq.com>
2022-01-05 20:16:57 +0800

    update:pycharm install

--- scripts/init_a_fresh_ubuntu/4.install_develop_softwares.sh
+++ scripts/init_a_fresh_ubuntu/4.install_develop_softwares.sh
@@ -1,5 +1,3 @@
-# sudo snap install pycharm-professional --classic
-# sudo snap install pycharm-community --classic
 # sudo snap install code --classic
 
 # sudo snap install firefox chromium typora meshlab
@@ -17,3 +15,11 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm *wps*
 wget https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/10702/wps-office_11.1.0.10702_amd64.deb
 sudo dpkg -i wps-office_11.1.0.10702_amd64.deb
+
+# pycharm-community
+# rm *pycharm*
+# wget wget https://download-cdn.jetbrains.com/python/pycharm-community-2021.3.1.tar.gz
+# sudo snap install pycharm-professional --classic
+sudo snap install pycharm-community --classic
+
+
