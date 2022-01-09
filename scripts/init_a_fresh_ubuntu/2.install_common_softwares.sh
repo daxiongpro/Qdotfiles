db43bbc1a70e1eb37db7c6c6dbc36bcc63359d4e
daxiongpro <910660298@qq.com>
2022-01-09 19:25:27 +0800

    update: add remmina install

--- scripts/init_a_fresh_ubuntu/2.install_common_softwares.sh
+++ scripts/init_a_fresh_ubuntu/2.install_common_softwares.sh
@@ -3,11 +3,9 @@ if test "$(uname)" = "Darwin"; then
   echo not supported now on mac now!
 
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
-  softwares="git tmux vim autojump rdesktop zsh silversearcher-ag\
-		       ranger curl tree make htop nodejs npm\
-			   snapd docker docker.io docker-compose"
-  echo "Hi handsome user! I will install some very common softwares\
-		you may need such as $softwares"
+  softwares="git tmux vim autojump rdesktop zsh remmina ranger curl tree make snapd docker docker.io docker-compose"
+
+  echo "Hi handsome user! I will install some very common softwares you may need such as $softwares"
 
   sudo apt install -y $softwares
 fi
