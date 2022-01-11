4d6d47e824f258979c734f4ecf0cec25f6eccd97
daxiongpro <910660298@qq.com>
2022-01-11 20:11:10 +0800

    zsh hint

--- scripts/init_a_fresh_ubuntu/3.oh-my-zsh_install.sh
+++ scripts/init_a_fresh_ubuntu/3.oh-my-zsh_install.sh
@@ -5,6 +5,7 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 # cp ~/.Qdotfiles/zsh/.zshrc ~/.zshrc
 
 chsh -s /usr/bin/zsh
+exec zsh
 
 # cd $ZSH_CUSTOM/plugins
 cd ~/.oh-my-zsh/custom/plugins
@@ -12,3 +13,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 source ~/.zshrc
+
+echo "please reboot"
