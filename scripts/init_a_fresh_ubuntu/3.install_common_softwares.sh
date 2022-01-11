72d2a0da1c42b39ca2499a4fe51fb1daad2d7b26
daxiongpro <910660298@qq.com>
2022-01-11 20:32:56 +0800

    update: change order of the two files

--- scripts/init_a_fresh_ubuntu/3.install_common_softwares.sh
+++ scripts/init_a_fresh_ubuntu/3.install_common_softwares.sh
@@ -3,7 +3,7 @@ if test "$(uname)" = "Darwin"; then
   echo not supported now on mac now!
 
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
-  softwares="git tmux vim autojump rdesktop zsh ranger curl tree make snapd docker docker.io docker-compose"
+  softwares="git tmux vim autojump rdesktop ranger curl tree make snapd docker docker.io docker-compose"
 
   echo "Hi handsome user! I will install some very common softwares you may need such as $softwares"
 
