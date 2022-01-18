d16cfa2d80949f88665cdffabb65f7b5d4af53ff
daxiongpro <910660298@qq.com>
2022-01-18 09:57:08 +0800

    add: miniconda install

--- scripts/init_a_fresh_ubuntu/4.install_develop_softwares.sh
+++ scripts/init_a_fresh_ubuntu/4.install_develop_softwares.sh
@@ -25,3 +25,7 @@ sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
 sudo apt update
 sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret
 
+
+# miniconda
+bash ../../conda/install.sh
+
