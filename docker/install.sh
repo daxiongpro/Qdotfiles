510f148090a2ea5fbf221db86147348433b1c442
daxiongpro <910660298@qq.com>
2022-01-18 10:20:45 +0800

    update: docker install

--- docker/install.sh
+++ docker/install.sh
@@ -16,8 +16,15 @@ _install_nvidia_docker() {
   ) &&
     curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - &&
     curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+    
+  sudo cat <<EOF >/etc/docker/daemon.json
+  {
+      "registry-mirrors": ["http://hub-mirror.c.163.com"]
+  }
+EOF
   sudo apt-get update
   sudo apt-get install -y nvidia-docker2
+  sudo systemctl restart docker
 }
 
 if test ! $(which docker); then
