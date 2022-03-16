255ae9c36f1aa4cfe72fba6d75c2ad2515860581
daxiongpro <910660298@qq.com>
2022-03-16 15:30:21 +0800

    disk auto mount

--- scripts/init_a_fresh_ubuntu/6.hard_ware_setting.sh
+++ scripts/init_a_fresh_ubuntu/6.hard_ware_setting.sh
@@ -1,14 +1,21 @@
-for hardware in $(realpath *TB*); do
-  hardware_name=$(basename $hardware)
-  for user in $(ls /home); do
-    dir="$hardware"/"$user"
-    linkdir=/home/"$user"/"$hardware_name"
+# sudo fdisk -l
+# sudo lsblk
+sudo mount /dev/sda1 ~/1TB
+# 设置卡机自动挂载
+# 打开/etc/fstab, 设置如下：
+# /dev/sdb /mnt/1TB ext4 defaults 0 0
 
-    echo "$dir --> $linkdir"
-    mkdir -p $dir
-    ln -s $dir $linkdir
-
-    chown -R "$user":deepones $dir
-    chown -R "$user":deepones -h $linkdir
-  done
-done
+# for hardware in $(realpath *TB*); do
+#   hardware_name=$(basename $hardware)
+#   for user in $(ls /home); do
+#     dir="$hardware"/"$user"
+#     linkdir=/home/"$user"/"$hardware_name"
+# 
+#     echo "$dir --> $linkdir"
+#     mkdir -p $dir
+#     ln -s $dir $linkdir
+# 
+#     chown -R "$user":deepones $dir
+#     chown -R "$user":deepones -h $linkdir
+#   done
+# done
