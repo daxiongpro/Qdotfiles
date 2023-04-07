6eba76dea3e5ae0c4f5a75cba76ac7cc05ac733f
daxiongpro <910660298@qq.com>
2023-04-07 14:34:32 +0800

    update: install vscode without snap

--- scripts/init_a_fresh_ubuntu/2.install_develop_softwares.sh
+++ scripts/init_a_fresh_ubuntu/2.install_develop_softwares.sh
@@ -11,7 +11,6 @@ main() {
     rm *google*
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb 
-
   elif [ "$1" = "--edge" ]; then
     # microsoft edge
     sudo apt install -f -y
@@ -19,19 +18,15 @@ main() {
     wget https://go.microsoft.com/fwlink\?linkid\=2149051 
     mv 'fwlink?linkid=2149051' edge.deb
     sudo dpkg -i edge.deb
-
- 
   elif [ "$1" = "--wps" ]; then 
     # wps
     rm *wps*
     wget https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/10702/wps-office_11.1.0.10702_amd64.deb
     sudo dpkg -i wps-office_11.1.0.10702_amd64.deb
-
-
   elif [ "$1" = "--pycharm-community" ]; then
     # pycharm-community
     sudo snap install pycharm-community --classic
-    elif [ "$1" = "--pycharm-professional" ]; then
+  elif [ "$1" = "--pycharm-professional" ]; then
     # pycharm-professional
     sudo snap install pycharm-professional --classic
   elif [ "$1" = "--remmina" ]; then
@@ -44,8 +39,14 @@ main() {
     # miniconda
     bash ~/.Qdotfiles/conda/install.sh
   elif [ "$1" = "--vscode" ]; then
-    # vscode
-    sudo snap install code --classic
+    # vscode 
+    # reference: https://blog.csdn.net/sunzhao1000/article/details/120904566
+    sudo apt update
+    sudo apt install software-properties-common apt-transport-https wget
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt update
+    sudo apt install code
   elif [ "$1" = "--wechat" ]; then
     # wechat
     sudo snap install electronic-wechat
@@ -54,7 +55,6 @@ main() {
     sudo add-apt-repository ppa:shutter/ppa
     sudo apt-get update
     sudo apt-get install shutter
-
   else
     cat <<EOF
     Install some develope softwares
